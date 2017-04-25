7c59a48584cf8727449966be89c5b9fed2fb4f18
Yannick Blondeau <yannick.blondeau@laposte.net>
2017-04-25 19:12:49 +0200

    Move amenities to resource file

--- ExpediaHackathon.js
+++ ExpediaHackathon.js
@@ -14,82 +14,9 @@ var ExpediaHackathonAPP = angular.module('ExpediaHackathon', [
 ExpediaHackathonAPP
 
     .run(function ($rootScope, $location, $http, $uibModal, $interval, $timeout) { 
-        $rootScope.featuredAmenityEnum = [
-        '2 Bathrooms',
-        'Allergy Friendly',
-        'Balcony',
-        'Bathtub',
-        'Business Lounge Access',
-        'Concierge Service',
-        'Connecting Rooms',
-        'Ensuite',
-        'Fireplace',
-        'Hot Tub',
-        'Jetted Tub',
-        'Kitchen',
-        'Kitchenette',
-        'Lanai',
-        'Microwave',
-        'No Windows',
-        'Patio',
-        'Pool Access',
-        'Private Bathroom',
-        'Private Pool',
-        'Refrigerator',
-        'Refrigerator & Microwave',
-        'Sauna',
-        'Shared Bathroom',
-        'Terrace'];
-        
-        $rootScope.viewEnum = [
-        'Bay View',
-        'Beach View',
-        'Canal View',
-        'City View',
-        'Courtyard View',
-        'Garden View',
-        'Golf View',
-        'Harbor View',
-        'Hill View',
-        'Lagoon View',
-        'Lake View',
-        'Marina View',
-        'Mountain View',
-        'Multiple View',
-        'No View',
-        'Ocean View',
-        'Park View',
-        'Partial Lake View',
-        'Partial Ocean View',
-        'Partial Sea View',
-        'Partial View',
-        'Pool View',
-        'Resort View',
-        'River View',
-        'Sea View',
-        'Valley View',
-        'View',
-        'Vineyard View'];
-        
-        $rootScope.areaEnum = [
-        'Attribute',
-        'Annex Building',
-        'Beachfront',
-        'Beachside',
-        'Corner',
-        'Courtyard Area',
-        'Executive Level',
-        'Garden Area',
-        'Ground Floor',
-        'Lakeside',
-        'Mezzanine',
-        'Mountainside',
-        'Oceanfront',
-        'Overwater',
-        'Poolside',
-        'Sea Facing',
-        'Slope side',
-        'Tower' ];
+        $rootScope.featuredAmenityEnum = featuredAmenitiesFullList;
+
+        $rootScope.viewEnum = viewsFullList;
 
         var autocomplete;
 
