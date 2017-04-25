cabacb050ddff2c5f55683f222e33a2e85f89b91
christopher <christopher.meignier@hotel-spider.com>
2017-04-25 15:25:08 +0200

    Better UI

--- ExpediaHackathon.js
+++ ExpediaHackathon.js
@@ -9,7 +9,86 @@ var ExpediaHackathonAPP = angular.module('ExpediaHackathon', [
 ]);
 
 ExpediaHackathonAPP
-    .run(function ($rootScope, $location, $http, $uibModal, $interval) { 
+    .run(function ($rootScope, $location, $http, $uibModal, $interval) {
+        
+        
+        $rootScope.featuredAmenityEnum = [
+        '2 Bathrooms',
+        'Allergy Friendly',
+        'Balcony',
+        'Bathtub',
+        'Business Lounge Access',
+        'Concierge Service',
+        'Connecting Rooms',
+        'Ensuite',
+        'Fireplace',
+        'Hot Tub',
+        'Jetted Tub',
+        'Kitchen',
+        'Kitchenette',
+        'Lanai',
+        'Microwave',
+        'No Windows',
+        'Patio',
+        'Pool Access',
+        'Private Bathroom',
+        'Private Pool',
+        'Refrigerator',
+        'Refrigerator & Microwave',
+        'Sauna',
+        'Shared Bathroom',
+        'Terrace'];
+        
+        $rootScope.viewEnum = [
+        'Bay View',
+        'Beach View',
+        'Canal View',
+        'City View',
+        'Courtyard View',
+        'Garden View',
+        'Golf View',
+        'Harbor View',
+        'Hill View',
+        'Lagoon View',
+        'Lake View',
+        'Marina View',
+        'Mountain View',
+        'Multiple View',
+        'No View',
+        'Ocean View',
+        'Park View',
+        'Partial Lake View',
+        'Partial Ocean View',
+        'Partial Sea View',
+        'Partial View',
+        'Pool View',
+        'Resort View',
+        'River View',
+        'Sea View',
+        'Valley View',
+        'View',
+        'Vineyard View'];
+        
+        $rootScope.areaEnum = [
+        'Attribute',
+        'Annex Building',
+        'Beachfront',
+        'Beachside',
+        'Corner',
+        'Courtyard Area',
+        'Executive Level',
+        'Garden Area',
+        'Ground Floor',
+        'Lakeside',
+        'Mezzanine',
+        'Mountainside',
+        'Oceanfront',
+        'Overwater',
+        'Poolside',
+        'Sea Facing',
+        'Slope side',
+        'Tower' ];
+        
     })
     .config(function($httpProvider, $base64) {
         var auth = $base64.encode("foo:bar");
@@ -150,6 +229,220 @@ ExpediaHackathonAPP
         $timeout( function(){
             $scope.initializePhysicalAutoComplete();
         }, 1000);
+        
+        
+        
+        
+        $scope.Property = {
+            "providerPropertyId": "1289472",
+            "name": "Peach Inn",
+            "latitude": "23.3752",
+            "longitude": "-81.3261",
+            "providerPropertyUrl": "http://example.org",
+            "structureType": "Hotel",
+            "currencyCode": "USD",
+            "billingCurrencyCode": "USD",
+            "timeZone": "America/Los_Angeles",
+            "addresses": [
+                {
+                    "line1": "123 Main St.",
+                    "city": "B. Hills",
+                    "state": "CA",
+                    "postalCode": "90210",
+                    "countryCode": "USA"
+                }
+            ],
+            "ratings": [
+                {
+                    "score": 4,
+                    "maxScore": 5,
+                    "source": "AAA",
+                    "description": "STARS"
+                }
+            ],
+            "contacts": {
+                "Property": {
+                    "phoneNumbers": [
+                        {
+                            "phoneNumberType": "Phone",
+                            "countryAccessCode": "1",
+                            "areaCode": "123",
+                            "number": "1234567"
+                        }
+                    ]
+                },
+                "ReservationManager": {
+                    "firstName": "First",
+                    "lastName": "Last",
+                    "emails": [
+                        "abc@xyz.com"
+                    ],
+                    "phoneNumbers": [
+                        {
+                            "phoneNumberType": "Phone",
+                            "countryAccessCode": "1",
+                            "areaCode": "123",
+                            "number": "4567890"
+                        },
+                        {
+                            "phoneNumberType": "Fax",
+                            "countryAccessCode": "1",
+                            "areaCode": "123",
+                            "number": "7890123"
+                        }
+                    ]
+                },
+                "AlternateReservationManager": {
+                    "phoneNumbers": [
+                        {
+                            "phoneNumberType": "Phone",
+                            "countryAccessCode": "1",
+                            "areaCode": "123",
+                            "number": "1234567"
+                        }
+                    ]
+                },
+                "GeneralManager": {
+                    "firstName": "General",
+                    "lastName": "Manager",
+                    "emails": [
+                        "abc123@xyz.com"
+                    ]
+                }
+            },
+            "contents": [
+                {
+                    "locale": "en-US",
+                    "name": "Localized Property Name",
+                    "providerPropertyUrl": null,
+                    /*"images": [
+                        {
+                            "url": "http://images.xyz.com/mainImage.jpg",
+                            "categoryCode": "FEATURED_IMAGE",
+                            "caption": "Main Image"
+                        }
+                    ],
+                    "amenities": [
+                          {
+                            "code": "WIFI_INTERNET",
+                            "detailCode": "SURCHARGE_PER_STAY",
+                            "value": 10.99
+                        }
+                    ],
+                    "paragraphs": [
+                        {
+                            "code": "DESCRIPTION",
+                            "value": "Property description."
+                        },
+                        {
+                            "code": "SPECIAL_CHECKIN_INSTRUCTIONS",
+                            "value": "Special check-in instructions."
+                        }
+                    ]*/
+                }
+            ],
+            "propertyCollectedMandatoryFees": [
+                {
+                    "code": "RESORT_FEE",
+                    "scope": "PER_PERSON",
+                    "duration": "PER_NIGHT",
+                    "value": 25.99,
+                    "startDate": null,
+                    "endDate": null
+                }
+            ],
+            "taxes": [
+                {
+                    "code": "VAT",
+                    "detailCode": "PERCENT_PER_STAY",
+                    "value": 20
+                }
+            ],
+            "policies": [
+                {
+                    "code": "MINIMUM_CHECKIN_AGE",
+                    "value": "18"
+                }
+            ],
+            "inventorySettings": {
+                "rateAcquisitionType": "NET_RATE",
+                "distributionModels": [
+                    "EXPEDIA_COLLECT",
+                    "HOTEL_COLLECT"
+                ]
+            },
+            "attributes": [
+                {
+                    "code": "TOTAL_ROOMS",
+                    "value": "15"
+                }
+            ]
+        };
+        
+        $scope.PropertyRoom = {
+            "partnerCode": "MyStringCode",
+            "name": {
+                "attributes": {
+                    "typeOfRoom": "Penthouse",
+                    "roomClass": "Executive",
+                    "includeBedType": true,
+                    "view": "City View",
+                    "featuredAmenity": "Jetted Tub",
+                    "customLabel": "Rooftop Terrace"
+                }
+            },
+            "ageCategories": [
+                {
+                    "category": "Adult", 
+                    "minAge": 18
+                }, 
+                {
+                    "category": "ChildAgeA", 
+                    "minAge": 6
+                }, 
+                {
+                    "category": "Infant", 
+                    "minAge": 0
+                }
+            ], 
+            "maxOccupancy": {
+                "adults": 2, 
+                "children": 1, 
+                "total": 3
+            }, 
+            "standardBedding": [
+                {
+                    "option": [
+                        {
+                            "quantity": 1,
+                            "type": "King Bed"
+                        }
+                    ]
+                }
+            ], 
+            "extraBedding": [
+                {
+                    "quantity": 1,
+                    "type": "Rollaway Bed",
+                    "size": "Full",
+                    "surcharge": {
+                        "type": "Per Day",
+                        "amount": 20
+                    }
+                }
+            ],
+            "smokingPreferences": [
+                "Non-Smoking"
+            ],
+            "roomSize": {
+                "squareFeet": 300,
+                "squareMeters": 14
+            },
+            "views": [
+                "Beach View"
+            ]
+        };
+    
             
             
     }])
